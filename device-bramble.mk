22b3f0a708a43a7c1b761d684fb791df7b00d115
maade93791 <70593890+maade69@users.noreply.github.com>
2024-07-09 15:44:10 +0300

    build com.android.vndk.current.on_vendor

--- device-bramble.mk
+++ device-bramble.mk
@@ -216,3 +216,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
 PRODUCT_PACKAGES += bramble_product_compatibility_matrix.xml
+
+PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
